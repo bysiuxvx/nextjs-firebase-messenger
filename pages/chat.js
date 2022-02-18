1fdebe7e63d4258c853fb6281277c4d7d03ccd51
Patryk <byszek.patryk@gmail.com>
2022-02-18 23:47:00 +0100

    Upd: fixing bugs

--- pages/chat.js
+++ pages/chat.js
@@ -1,33 +1,152 @@
+// import React, { useEffect, useRef, useState } from "react"
+// import { auth } from "../src/firebase"
+// import { history } from "../src/helpers/helper"
+// import { Container, Card, Segment, Button, Grid } from "semantic-ui-react"
+
+// import dynamic from "next/dynamic"
+// import Image from "next/image"
+// import { useRouter } from "next/router"
+// import { useAuth } from "../src/contexts/AuthContext"
+// import axios from "axios"
+
+// import logo_svg from "../src/svg/bullterrier.svg"
+
+// import "../node_modules/react-quill/dist/quill.snow.css"
+
+// const ChatEngine = dynamic(() =>
+//   import("nextjs-chat-engine").then((module) => module.ChatEngine)
+// )
+// const MessageFormSocial = dynamic(() =>
+//   import("nextjs-chat-engine").then((module) => module.MessageFormSocial)
+// )
+
+// const Chat = () => {
+//   const { user } = useAuth()
+//   const [loading, setLoading] = useState(true)
+//   const [showChat, setShowChat] = useState(false)
+
+//   const router = useRouter()
+
+//   const handleLogout = async () => {
+//     await auth.signOut()
+//     setShowChat(false)
+//   }
+
+//   const getFile = async (url) => {
+//     const response = await fetch(url)
+//     const data = await response.blob()
+//     return new File([data], "userPhoto.jpg", { type: "image/jpeg" })
+//   }
+
+//   useEffect(() => {
+//     if (!user) {
+//       router.push("/login")
+//       return
+//     }
+
+//     axios
+//       .get("https://api.chatengine.io/users/me", {
+//         headers: {
+//           "project-id": "22cfe9ea-f525-4c6f-be4a-75ae67d95a40",
+//           // "project-id": process.env.APP_CHAT_ENGINE_ID,
+//           "user-name": user.email,
+//           "user-secret": user.uid,
+//         },
+//       })
+//       .then(() => {
+//         setLoading(false)
+//         setShowChat(true)
+//       })
+//       .catch(() => {
+//         let formData = new FormData()
+//         formData.append("email", user.email)
+//         formData.append("username", user.email)
+//         formData.append("secret", user.uid)
+//         getFile(user.photoURL).then((avatar) => {
+//           formData.append("avatar", avatar, avatar.name)
+//           axios
+//             .post("https://api.chatengine.io/users", formData, {
+//               headers: {
+//                 "private-key": "38aa251f-bc59-422a-b212-a10dd39f45e6",
+//                 // "private-key": process.env.APP_CHAT_ENGINE_KEY,
+//               },
+//             })
+//             .then(() => {
+//               setLoading(false)
+//             })
+//             .catch((error) => console.log(error))
+//         })
+//       })
+//   }, [user])
+
+//   useEffect(() => {
+//     if (typeof document !== null) {
+//       setShowChat(true)
+//     }
+//   }, [showChat])
+
+//   if (!user && loading && showChat) return "Please wait..."
+//   if (!user && !showChat) return <div />
+
+//   return (
+//     <Container className="chat-page">
+//       <Segment className="chat-nav">
+//         <Container className="logo">
+//           <Image src={logo_svg} height={80} alt="messenger logo" />
+//         </Container>
+//         <Button onClick={handleLogout}>Logout</Button>
+//       </Segment>
+//       <ChatEngine
+//         height="calc(100vh - 150px)"
+//         projectID="22cfe9ea-f525-4c6f-be4a-75ae67d95a40"
+//         // projectID={process.env.APP_CHAT_ENGINE_ID}
+//         userName={user ? user.email : null}
+//         userSecret={user ? user.uid : null}
+//         MessageFormSocial={() => <MessageFormSocial />}
+//       />
+//     </Container>
+//   )
+// }
+
+// export default Chat
+
 import React, { useEffect, useRef, useState } from "react"
-import { Avatar, ChatEngine } from "nextjs-chat-engine"
 import { auth } from "../src/firebase"
 import { history } from "../src/helpers/helper"
-import { Container, Card, Segment, Button } from "semantic-ui-react"
+import { Container, Card, Segment, Button, Grid } from "semantic-ui-react"
 
 import dynamic from "next/dynamic"
-
+import Image from "next/image"
 import { useRouter } from "next/router"
 import { useAuth } from "../src/contexts/AuthContext"
 import axios from "axios"
 
-require("react-quill/dist/quill.snow.css")
+import logo_svg from "../src/svg/bullterrier.svg"
+
+import "../node_modules/react-quill/dist/quill.snow.css"
+
+const ChatEngine = dynamic(() =>
+  import("nextjs-chat-engine").then((module) => module.ChatEngine)
+)
+const MessageFormSocial = dynamic(() =>
+  import("nextjs-chat-engine").then((module) => module.MessageFormSocial)
+)
 
 const Chat = () => {
-  const router = useRouter()
   const { user } = useAuth()
   const [loading, setLoading] = useState(true)
+  const [showChat, setShowChat] = useState(false)
 
-  console.log(user)
+  const router = useRouter()
 
   const handleLogout = async () => {
     await auth.signOut()
-    router.push("/")
+    setShowChat(false)
   }
 
   const getFile = async (url) => {
     const response = await fetch(url)
     const data = await response.blob()
-
     return new File([data], "userPhoto.jpg", { type: "image/jpeg" })
   }
 
@@ -40,13 +159,16 @@ const Chat = () => {
     axios
       .get("https://api.chatengine.io/users/me", {
         headers: {
-          "project-id": "22cfe9ea-f525-4c6f-be4a-75ae67d95a40",
-          // "project-id": process.env.APP_CHAT_ENGINE_ID,
+          // "project-id": "22cfe9ea-f525-4c6f-be4a-75ae67d95a40",
+          "project-id": process.env.APP_CHAT_ENGINE_ID,
           "user-name": user.email,
           "user-secret": user.uid,
         },
       })
-      .then(() => setLoading(false))
+      .then(() => {
+        setLoading(false)
+        setShowChat(true)
+      })
       .catch(() => {
         let formData = new FormData()
         formData.append("email", user.email)
@@ -57,32 +179,42 @@ const Chat = () => {
           axios
             .post("https://api.chatengine.io/users", formData, {
               headers: {
-                "private-key": "38aa251f-bc59-422a-b212-a10dd39f45e6",
-                // "private-key": process.env.APP_CHAT_ENGINE_KEY,
+                // "private-key": "38aa251f-bc59-422a-b212-a10dd39f45e6",
+                "private-key": process.env.APP_CHAT_ENGINE_KEY,
               },
             })
-            .then(() => setLoading(false))
+            .then(() => {
+              setLoading(false)
+            })
             .catch((error) => console.log(error))
         })
       })
-  }, [user, router.isReady])
+  }, [user])
 
-  if (!user || loading) return "Loading..."
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true)
+    }
+  }, [showChat])
+
+  if (!user && loading && showChat) return "Please wait..."
+  if (!user && !showChat) return <div />
 
   return (
-    <Container className="chat-page" fluid>
-      <Segment className="nav-head">
-        <Container className="logo"></Container>
-        <Container className="logout">
-          <Button onClick={handleLogout}>Logout</Button>
+    <Container className="chat-page">
+      <Segment className="chat-nav">
+        <Container className="logo">
+          <Image src={logo_svg} height={80} alt="messenger logo" />
         </Container>
+        <Button onClick={handleLogout}>Logout</Button>
       </Segment>
       <ChatEngine
         height="calc(100vh - 150px)"
-        projectID="22cfe9ea-f525-4c6f-be4a-75ae67d95a40"
-        // projectID={process.env.APP_CHAT_ENGINE_ID}
-        userName={user.email}
-        userSecret={user.uid}
+        // projectID="22cfe9ea-f525-4c6f-be4a-75ae67d95a40"
+        projectID={process.env.APP_CHAT_ENGINE_ID}
+        userName={user ? user.email : null}
+        userSecret={user ? user.uid : null}
+        MessageFormSocial={() => <MessageFormSocial />}
       />
     </Container>
   )
